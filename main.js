d8325784942bdde9a404cfe728f0ee41a1702a04
rexx0n <alexey.mironenko46@gmail.com>
2023-08-08 16:35:49 +0300

    made a new page, added a modal window

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 import "./scss/style.scss";
+
+let btnsNext = document.querySelectorAll('.computation__questions  .button-red')
+let questions = document.querySelectorAll('.computation__question')
+let paragraphs = document.querySelectorAll('.computation > p')
+let modal = document.querySelector('.modal')
+let count = 0
+btnsNext.forEach(btn => {
+    btn.addEventListener('click', () => {
+        paragraphs.forEach(paragraph => {
+            paragraph.classList.add('computation__question--none')
+        })
+        if (questions.length - 1 <= count) {
+            modal.classList.remove('modal--none')
+            modal.querySelector('svg').addEventListener('click', () => {
+                modal.classList.add('modal--none')
+            })
+        } else {
+            questions[count].classList.add('computation__question--none')
+            count++
+            questions[count].classList.remove('computation__question--none')
+        }
+    })
+})
